67f62c3f85ad92a8d88a488c1f05c3b1f3ca2b8c
setoharuka <persona0121@gmail.com>
2020-07-03 23:08:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,8 +5,17 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+ID_ROLE_MEMBER = 576891687620118902
 
-@bot.event
+@client.event
+async def on_member_join(member):
+    # 用意したIDから Role オブジェクトを取得
+    role = member.guild.get_role(ID_ROLE_MEMBER)
+
+    # 入ってきた Member に役職を付与
+    await member.add_roles(role)
+
+    @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
